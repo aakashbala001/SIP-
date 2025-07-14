d0a5d200a5e4d8c03fd4f65d7028b3c8bdb03652
aakashbala001 <akashbalaak001@gmail.com>
2025-07-14 19:17:54 +0530

    Create Factorial.java

---
+++ JavaStatementsAndLoops/while_loop/Factorial.java
@@ -0,0 +1,11 @@
+// 7.Find the factorial of a number
+public class Factorial {
+    public static void main(String[] args) {
+        int num = 5, fact = 1;
+        while (num > 0) {
+            fact *= num;
+            num--;
+        }
+        System.out.println("Factorial = " + fact);
+    }
+}
