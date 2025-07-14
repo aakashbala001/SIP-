92fdf9dc0b69c48ed4c2c530da0fcab72caec6fa
aakashbala001 <akashbalaak001@gmail.com>
2025-07-14 19:16:18 +0530

    Create Countdown.java

---
+++ JavaStatementsAndLoops/while_loop/Countdown.java
@@ -0,0 +1,10 @@
+// 8. Print a countdown from 10 to 1
+public class Countdown {
+    public static void main(String[] args) {
+        int i = 10;
+        while (i >= 1) {
+            System.out.println(i);
+            i--;
+        }
+    }
+}
