f36a1c687151d86e69c1c57c9f3039d3d74d2d02
aakashbala001 <akashbalaak001@gmail.com>
2025-07-14 20:16:47 +0530

    Create Discount.java

---
+++ JavaStatementsAndLoops/else_if_statement/Discount.java
@@ -0,0 +1,14 @@
+// 9.Assign discount based on purchase amount
+public class Discount {
+    public static void main(String[] args) {
+        double amount = 3500;
+
+        if (amount >= 5000) {
+            System.out.println("20% Discount");
+        } else if (amount >= 2000) {
+            System.out.println("10% Discount");
+        } else {
+            System.out.println("No Discount");
+        }
+    }
+}
