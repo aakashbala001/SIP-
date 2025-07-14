d8710437947e2039c5e82fe23582f989eec4cfdb
aakashbala001 <akashbalaak001@gmail.com>
2025-07-14 20:25:15 +0530

    Create PasswordCheck.java

---
+++ JavaStatementsAndLoops/if_statement/PasswordCheck.java
@@ -0,0 +1,11 @@
+// 7.Validate if a password length is at least 8 characters
+public class PasswordCheck {
+    public static void main(String[] args) {
+        String password = "mypassword";
+        if (password.length() >= 8) {
+            System.out.println("Password is valid.");
+        } else {
+            System.out.println("Password is too short.");
+        }
+    }
+}
