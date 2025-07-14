289e310faa68fd538e1a255ff5972831051d0514
aakashbala001 <akashbalaak001@gmail.com>
2025-07-14 19:37:38 +0530

    Create Sequence.java

---
+++ JavaStatementsAndLoops/do_while_loop/Sequence.java
@@ -0,0 +1,10 @@
+// 10.Print a sequence increasing by 5 up to 50
+public class Sequence {
+    public static void main(String[] args) {
+        int i = 5;
+        do {
+            System.out.println(i);
+            i += 5;
+        } while (i <= 50);
+    }
+}
