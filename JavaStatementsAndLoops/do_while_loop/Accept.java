91f23c2c4c4a8478e8aa4e7daddb148c83c00379
aakashbala001 <akashbalaak001@gmail.com>
2025-07-14 19:24:44 +0530

    Create Accept.java

---
+++ JavaStatementsAndLoops/do_while_loop/Accept.java
@@ -0,0 +1,13 @@
+// 9.Keep accepting numbers from the user until they enter zero
+import java.util.Scanner;
+public class Accept {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        do {
+            System.out.print("Enter a number (0 to stop): ");
+            num = sc.nextInt();
+        } while (num != 0);
+        System.out.println("Program stopped.");
+    }
+}
