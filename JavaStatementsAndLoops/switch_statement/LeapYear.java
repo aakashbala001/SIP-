ade5679aeabaaaed3d9253b79257d92269684767
aakashbala001 <akashbalaak001@gmail.com>
2025-07-14 20:29:09 +0530

    Create LeapYear.java

---
+++ JavaStatementsAndLoops/switch_statement/LeapYear.java
@@ -0,0 +1,14 @@
+// 5.Check if a year is a leap year
+import java.util.Scanner;
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = sc.nextInt();
+
+        switch ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0 ? 1 : 0) {
+            case 1: System.out.println(year + " is a leap year"); break;
+            case 0: System.out.println(year + " is not a leap year"); break;
+        }
+    }
+}
