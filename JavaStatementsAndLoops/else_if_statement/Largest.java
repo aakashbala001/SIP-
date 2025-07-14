9f6f325ec209f453c5af40c7634f9ae8ef80f70d
aakashbala001 <akashbalaak001@gmail.com>
2025-07-14 20:17:59 +0530

    Create Largest.java

---
+++ JavaStatementsAndLoops/else_if_statement/Largest.java
@@ -0,0 +1,13 @@
+// 5.Find the largest of three numbers
+public class Largest {
+    public static void main(String[] args) {
+        int a = 15, b = 20, c = 10;
+        if (a >= b && a >= c) {
+            System.out.println(a + " is the largest");
+        } else if (b >= a && b >= c) {
+            System.out.println(b + " is the largest");
+        } else {
+            System.out.println(c + " is the largest");
+        }
+    }
+}
