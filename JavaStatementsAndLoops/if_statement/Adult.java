511a7d04e409fb59ed9327519dd5384d69582bc8
aakashbala001 <akashbalaak001@gmail.com>
2025-07-14 20:22:28 +0530

    Create Adult.java

---
+++ JavaStatementsAndLoops/if_statement/Adult.java
@@ -0,0 +1,11 @@
+// 5.Determine if a person is adult or not
+public class Adult {
+    public static void main(String[] args) {
+        int age = 17;
+        if (age >= 18) {
+            System.out.println("Person is an adult.");
+        } else {
+            System.out.println("Person is not an adult.");
+        }
+    }
+}
