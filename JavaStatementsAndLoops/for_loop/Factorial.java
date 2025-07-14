76b2462798cc81eeeb17c276cf621ad8c730110a
aakashbala001 <akashbalaak001@gmail.com>
2025-07-14 19:57:03 +0530

    Create Factorial.java

---
+++ JavaStatementsAndLoops/for_loop/Factorial.java
@@ -0,0 +1,10 @@
+// 6.Find the factorial of a number
+public class Factorial {
+    public static void main(String[] args) {
+        int num = 5, fact = 1;
+        for (int i = 1; i <= num; i++) {
+            fact *= i;
+        }
+        System.out.println("Factorial of " + num + " = " + fact);
+    }
+}
