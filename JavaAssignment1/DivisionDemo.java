135ec3c2231ee6712db606bbdc048e62e066bc21
aakashbala001 <akashbalaak001@gmail.com>
2025-07-14 20:39:28 +0530

    Create DivisionDemo.java

---
+++ JavaAssignment1/DivisionDemo.java
@@ -0,0 +1,9 @@
+public class DivisionDemo {
+    public static void main(String[] args) {
+        int intResult = 10 / 2 ;
+        double doubleResult = 8.0 / 2;
+
+        System.out.println("Integer division: " + intResult);       
+        System.out.println("Double division: " + doubleResult);    
+    }
+}
