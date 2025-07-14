74a92806a50a2dba7e67f7fb1d021ef30acf3e4e
aakashbala001 <akashbalaak001@gmail.com>
2025-07-14 20:04:06 +0530

    Create OddNumbers.java

---
+++ JavaStatementsAndLoops/for_loop/OddNumbers.java
@@ -0,0 +1,8 @@
+// 3.Print odd numbers from 1 to 15
+public class OddNumbers {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 15; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
