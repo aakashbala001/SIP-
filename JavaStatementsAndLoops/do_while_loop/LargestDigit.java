8900c6c7548360cedc5d7a88fcaff46e3c7363e4
aakashbala001 <akashbalaak001@gmail.com>
2025-07-14 19:27:51 +0530

    Create LargestDigit.java

---
+++ JavaStatementsAndLoops/do_while_loop/LargestDigit.java
@@ -0,0 +1,14 @@
+// 8. Find the largest digit in a number
+public class LargestDigit {
+    public static void main(String[] args) {
+        int num = 9583, largest = 0;
+        do {
+            int digit = num % 10;
+            if (digit > largest) {
+                largest = digit;
+            }
+            num /= 10;
+        } while (num != 0);
+        System.out.println("Largest digit = " + largest);
+    }
+}
