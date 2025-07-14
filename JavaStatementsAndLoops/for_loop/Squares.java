2fa192ffe2d5a58a253f88f34bb5915c887763f2
aakashbala001 <akashbalaak001@gmail.com>
2025-07-14 20:05:50 +0530

    Create Squares.java

---
+++ JavaStatementsAndLoops/for_loop/Squares.java
@@ -0,0 +1,8 @@
+// 10.Print squares of numbers from 1 to 10
+public class Squares {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + " squared = " + (i * i));
+        }
+    }
+}
