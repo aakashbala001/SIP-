2b0220bc00b74aad8edd5a033fb7dca6c4bcdded
aakashbala001 <akashbalaak001@gmail.com>
2025-07-14 19:39:10 +0530

    Create SumOfDigits.java

---
+++ JavaStatementsAndLoops/do_while_loop/SumOfDigits.java
@@ -0,0 +1,11 @@
+// 7. Calculate the sum of digits of a number
+public class SumOfDigits {
+    public static void main(String[] args) {
+        int num = 456, sum = 0;
+        do {
+            sum += num % 10;
+            num /= 10;
+        } while (num != 0);
+        System.out.println("Sum of digits = " + sum);
+    }
+}
