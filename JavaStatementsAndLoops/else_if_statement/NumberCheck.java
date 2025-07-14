6bca3ef082875afcc8cd37f9d4b7cb290c6a9c73
aakashbala001 <akashbalaak001@gmail.com>
2025-07-14 20:18:29 +0530

    Create NumberCheck.java

---
+++ JavaStatementsAndLoops/else_if_statement/NumberCheck.java
@@ -0,0 +1,13 @@
+// 1.Check if a number is positive, negative, or zero
+public class NumberCheck {
+    public static void main(String[] args) {
+        int num = 0;
+        if (num > 0) {
+            System.out.println("Positive");
+        } else if (num < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
