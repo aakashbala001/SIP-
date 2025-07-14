a94bba1608238e6f52471d9bbaa3b8a874edafe8
aakashbala001 <akashbalaak001@gmail.com>
2025-07-14 19:31:32 +0530

    Create Print.java

---
+++ JavaStatementsAndLoops/do_while_loop/Print.java
@@ -0,0 +1,10 @@
+// 1.Print numbers from 1 to 10
+public class Print {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 10);
+    }
+}
