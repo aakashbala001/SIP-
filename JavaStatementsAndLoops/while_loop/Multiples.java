f553a543a116582bc85d440f016d219ab10e0b9c
aakashbala001 <akashbalaak001@gmail.com>
2025-07-14 19:18:37 +0530

    Create Multiples.java

---
+++ JavaStatementsAndLoops/while_loop/Multiples.java
@@ -0,0 +1,11 @@
+// 2. Print the first 5 multiples of 3
+public class Multiples {
+    public static void main(String[] args) {
+        int i = 1, count = 0;
+        while (count < 5) {
+            System.out.println(3 * i);
+            i++;
+            count++;
+        }
+    }
+}
