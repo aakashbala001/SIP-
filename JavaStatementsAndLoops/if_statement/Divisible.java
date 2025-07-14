1c566e501b4702cefc79412a51f61a28d999978e
aakashbala001 <akashbalaak001@gmail.com>
2025-07-14 20:23:01 +0530

    Create Divisible.java

---
+++ JavaStatementsAndLoops/if_statement/Divisible.java
@@ -0,0 +1,9 @@
+// 8.Check if a number is divisible by 5
+public class Divisible {
+    public static void main(String[] args) {
+        int num = 25;
+        if (num % 5 == 0) {
+            System.out.println(num + " is divisible by 5.");
+        }
+    }
+}
