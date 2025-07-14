ab8746855164945595d3a01df193d8fc1b728e55
aakashbala001 <akashbalaak001@gmail.com>
2025-07-14 20:05:10 +0530

    Create Reverse.java

---
+++ JavaStatementsAndLoops/for_loop/Reverse.java
@@ -0,0 +1,8 @@
+// 7.Print numbers from 10 to 1 in reverse order
+public class Reverse {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
