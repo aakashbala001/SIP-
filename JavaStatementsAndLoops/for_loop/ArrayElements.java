77b5dd2c57503037b4351d29436534356c258387
aakashbala001 <akashbalaak001@gmail.com>
2025-07-14 19:54:13 +0530

    Create ArrayElements.java

---
+++ JavaStatementsAndLoops/for_loop/ArrayElements.java
@@ -0,0 +1,9 @@
+// 9.Display all elements of an array using a loop
+public class ArrayElements {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
