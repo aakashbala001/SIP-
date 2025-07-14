c3c7a71220009ebd46cd3284fc3b732729b7179f
aakashbala001 <akashbalaak001@gmail.com>
2025-07-14 20:27:26 +0530

    Create Vowel.java

---
+++ JavaStatementsAndLoops/if_statement/Vowel.java
@@ -0,0 +1,11 @@
+// 6.Check if a given character is vowel
+public class Vowel {
+    public static void main(String[] args) {
+        char ch = 'e';
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            System.out.println(ch + " is a vowel.");
+        } else {
+            System.out.println(ch + " is not a vowel.");
+        }
+    }
+}
