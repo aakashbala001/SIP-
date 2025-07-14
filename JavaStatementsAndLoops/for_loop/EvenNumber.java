e0d28647b685db2e1f8e6435057c97bd91acb8f1
aakashbala001 <akashbalaak001@gmail.com>
2025-07-14 19:55:27 +0530

    Create EvenNumber.java

---
+++ JavaStatementsAndLoops/for_loop/EvenNumber.java
@@ -0,0 +1,8 @@
+// 2.Print even numbers between 1 and 20
+public class EvenNumber {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 20; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
