e13335f9e6c6235fe1e8439281f205d143866a35
aakashbala001 <akashbalaak001@gmail.com>
2025-07-14 20:16:19 +0530

    Create CharCheck.java

---
+++ JavaStatementsAndLoops/else_if_statement/CharCheck.java
@@ -0,0 +1,17 @@
+// 10.Check if a character is a vowel, consonant, or not a letter
+public class CharCheck {
+    public static void main(String[] args) {
+        char ch = 'b';
+
+        if (Character.isLetter(ch)) {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+                System.out.println(ch + " is a vowel.");
+            } else {
+                System.out.println(ch + " is a consonant.");
+            }
+        } else {
+            System.out.println(ch + " is not a letter.");
+        }
+    }
+}
