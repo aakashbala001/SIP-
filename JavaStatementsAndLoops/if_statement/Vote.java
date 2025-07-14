038d004f75af8bf97e8398ea82d8e804dd4333ba
aakashbala001 <akashbalaak001@gmail.com>
2025-07-14 20:26:52 +0530

    Create Vote.java

---
+++ JavaStatementsAndLoops/if_statement/Vote.java
@@ -0,0 +1,9 @@
+// 3.Verify if a user is eligible to vote
+public class Vote {
+    public static void main(String[] args) {
+        int age = 20;
+        if (age >= 18) {
+            System.out.println("Eligible to vote.");
+        }
+    }
+}
