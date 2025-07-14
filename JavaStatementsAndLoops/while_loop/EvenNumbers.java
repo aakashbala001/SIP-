3dc4225fc3a39751ca627adbcd17e1e56f5d1343
aakashbala001 <akashbalaak001@gmail.com>
2025-07-14 19:17:12 +0530

    Create EvenNumbers.java

---
+++ JavaStatementsAndLoops/while_loop/EvenNumbers.java
@@ -0,0 +1,10 @@
+// 6.Print all even numbers up to 20
+public class EvenNumbers {
+    public static void main(String[] args) {
+        int i = 2;
+        while (i <= 20) {
+            System.out.println(i);
+            i += 2;
+        }
+    }
+}
