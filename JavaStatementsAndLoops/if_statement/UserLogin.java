1dbb9c23407db4921fb281f70746fadf0cc3f9db
aakashbala001 <akashbalaak001@gmail.com>
2025-07-14 20:26:28 +0530

    Create UserLogin.java

---
+++ JavaStatementsAndLoops/if_statement/UserLogin.java
@@ -0,0 +1,11 @@
+// 9.Verify if a user is logged in or not
+public class UserLogin {
+    public static void main(String[] args) {
+        boolean isLoggedIn = true;
+        if (isLoggedIn) {
+            System.out.println("User is logged in.");
+        } else {
+            System.out.println("User is not logged in.");
+        }
+    }
+}
