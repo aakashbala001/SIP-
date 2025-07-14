9f648f8c6548c3be9c0a242102a101794e62237a
aakashbala001 <akashbalaak001@gmail.com>
2025-07-14 20:25:56 +0530

    Create PositiveCheck.java

---
+++ JavaStatementsAndLoops/if_statement/PositiveCheck.java
@@ -0,0 +1,9 @@
+//Check if a number is positive
+public class PositiveCheck {
+    public static void main(String[] args) {
+        int num = 5;
+        if (num > 0) {
+            System.out.println("The number is positive.");
+        }
+    }
+}
